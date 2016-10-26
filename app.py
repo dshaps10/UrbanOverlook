1e7f448cc001b669646b8d85cf55a500b72cc8db
dshaps10 <dan.shapiro1210@gmail.com>
2016-10-26 13:16:52 -0700

    Add route for places page

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def main():
 	return render_template('index.html')
 
+@app.route("/places")
+def places():
+	return render_template('places.html')
+
 if __name__ == "__main__":
 	app.run()
